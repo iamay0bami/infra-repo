f3706a1bc099fc3e8f93e32cfab8aa22a9dcabc0
Ayobami Agboola <iamuskar26@gmail.com>
2026-02-10 15:41:55 +0000

    created terraform directory for eks provisioning

---
+++ terraform/eks.tf
@@ -0,0 +1,20 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
+
+  cluster_name    = "online-boutique-cluster"
+  cluster_version = "1.27"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_groups = {
+    main = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+      instance_types = ["t3.medium"] # Small enough to stay low cost, big enough for microservices
+    }
+  }
+}
